be9c27facdbd9ca8676105389a6475b3c9ab4547
Evan Liang <evliang@ucsc.edu>
2026-04-02 22:45:52 -0700

    changing url

--- site/frontend/src/App.jsx
+++ site/frontend/src/App.jsx
@@ -89,7 +89,7 @@ export default function App() {
   useEffect(() => {
     // Port 3001 is where the Node Express server will list. 
     // Usually configured via env, assuming localhost for testing.
-    const source = new EventSource('https://davis-collegiates.onrender.com');
+    const source = new EventSource('https://davis-collegiates.onrender.com/api/stream');
 
     source.onmessage = (e) => {
       try {
