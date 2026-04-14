abcf47f0e27a88f906ec9581fec0f68e4a69b3e4
Evan Liang <evliang@ucsc.edu>
2026-04-13 19:04:44 -0700

    added another warmup + changed lunch timings

--- site/frontend/src/App.jsx
+++ site/frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 const SCHEDULE_CONFIG = {
-  1: { start: '08:30', lunchStart: '12:40', lunchEnd: '13:20' },
-  2: { start: '08:30', lunchStart: '12:12', lunchEnd: '12:52' },
-  3: { start: '09:00', lunchStart: '12:20', lunchEnd: '13:00' }
+  1: { start: '08:30', lunchStart: '12:19', lunchEnd: '12:39' },
+  2: { start: '08:30', lunchStart: '11:59', lunchEnd: '12:19' },
+  3: { start: '09:00', lunchStart: '12:06', lunchEnd: '12:26' }
 };
 
-const WARMUP_EVENTS = ['AFA142', 'IFA122', 'IFA181', 'AFA381', 'IFA302'];
+const WARMUP_EVENTS = ['AFA142', 'IFA122', 'IFA181', 'AFA381', 'IFA302', 'AFA101'];
 
 const getMsForTimeStr = (str) => {
   return new Date(`2026-04-25T${str}:00-07:00`).getTime();
